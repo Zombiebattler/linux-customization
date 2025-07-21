9e0ede82a40fde0eebdb48d9d3717b4c3e931b50
Leon <leonkoelmel@gmail.com>
2025-07-21 18:38:54 +0200

    created all

--- main.sh
+++ main.sh
@@ -1,1 +1,20 @@
 #!bin/bash
+
+# standard
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git zsh nala -y
+
+
+# Install oh-my-zsh + plugins & config >
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
+
+cp ~/.zshrc ~/.zshrc.backup
