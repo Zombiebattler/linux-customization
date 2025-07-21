9148a96503d85d5e032ffb5cd3a6d57b88fa5da2
Leon <leonkoelmel@gmail.com>
2025-07-21 19:03:04 +0200

    Update

--- main.sh
+++ main.sh
@@ -1,13 +1,23 @@
 #!bin/bash
 
+ZHS_CONFIG_URL="https://raw.githubusercontent.com/Zombiebattler/linux-customization/refs/heads/main/configs/zshrc"
+
 # standard
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git zsh nala -y
+sudo apt install git zsh nala unzip -y
 
 
-# Install oh-my-zsh + plugins & config >
+# Nerdfonts
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
+unzip Meslo.zip -d ~/.local/share/fonts/
+fc-cache -fv
+rm Meslo.zip
+
+
+# oh-my-zsh + plugins & config >
 
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
@@ -17,4 +27,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
 
-cp ~/.zshrc ~/.zshrc.backup
+cp "$HOME/.zshrc" "$HOME/.zshrc.backup"
+curl -fsSL "$ZHS_CONFIG_URL" -o "$HOME/.zshrc"
+
+
+# Done.
+
+echo ""
+echo "✅ Done"
+echo "⚠️ First, configure the < Meslo > font in your terminal, then run < source .zshrc. >"
